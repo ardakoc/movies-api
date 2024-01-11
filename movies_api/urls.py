2ce11b3ec78898037b4ccd6dd10a414654c9f69f
Arda Koç <ardaskoc@gmail.com>
2024-01-11 15:45:55 +0300

    added three views of search operations for celery workers.

--- movies_api/urls.py
+++ movies_api/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from movies import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', views.search, name='search'),
+    path('search-wait/<uuid:result_uuid>/', views.search_wait, name='search_wait'),
+    path('search-results/', views.search_results, name='search_results'),
 ]
